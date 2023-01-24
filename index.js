730e0e7e56941a47363b64fa9c9ab1a1d2811e10
Matan Damary <matandamary10@gmail.com>
2023-01-24 21:17:06 +0200

    FIXED: Auth bugs - login, signup, logout

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const app = express();
-// const mongodb = require("mongodb");
 const dotenv = require("dotenv");
-dotenv.config();
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
 const productRoute = require("./routes/product");
@@ -10,42 +8,17 @@ const cartRoute = require("./routes/cart");
 const orderRoute = require("./routes/order");
 const stripeRoute = require("./routes/stripe");
 const cors = require("cors");
+dotenv.config();
 
-// const MongoClient = mongodb.MongoClient
-
-// const connectionParams = {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-//   tls: true,
-// }
-
-// mongoose
-//   .connect(process.env.MONGO_URL, connectionParams)
-//   .then(() => console.log("DB Connection Successfull!"))
-//   .catch((err) => console.log(err));
-
-// async function connectToDatabase() {
-//   const client = MongoClient.connect(process.env.MONGO_URL, connectionParams)
-//   const db = client.db('qleads')
-//   return db
-// }
-// connectToDatabase()
-// let client = new MongoClient(process.env.MONGO_URL, connectionParams)
-// client.connect().then((client) => {
-//   db = client.db("Test")
-//   db.collection("test").insertOne({ message: "Hello from DigitalOcean" }).then(() => {
-//     process.exit()
-//   })
-// })
 
 app.use(cors());
 app.use(express.json());
-// app.use("/api/auth", authRoute);
-// app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
-// app.use("/api/carts", cartRoute);
-// app.use("/api/orders", orderRoute);
-// app.use("/api/checkout", stripeRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
+app.use("/api/checkout", stripeRoute);
 
 
 
--- routes/auth.js
+++ routes/auth.js
@@ -4,21 +4,17 @@ const CryptoJS = require("crypto-js");
 const bcrypt = require('bcrypt')
 const jwt = require("jsonwebtoken");
 const dbService = require('../services/db.service')
+const userService = require('../services/user.service')
 
 //REGISTER
 router.post("/register", async (req, res) => {
   const { email, username, password } = JSON.parse(req.body.data)
-  const saltRounds = 10
-  const hash = await bcrypt.hash(password, saltRounds)
-  const newUser = new User({
-    username: username,
-    email: email,
-    password: hash
-  });
-
   try {
-    const savedUser = await newUser.save();
-    res.status(201).json(savedUser);
+    const saltRounds = 10
+    if (!username || !password) return Promise.reject('fullname, username and password are required!')
+    const hash = await bcrypt.hash(password, saltRounds)
+    let userToAdd = await userService.add({ username, password: hash, isAdmin: false, email })
+    res.status(201).json(userToAdd);
   } catch (err) {
     res.status(500).json(err);
   }
@@ -27,28 +23,28 @@ router.post("/register", async (req, res) => {
 //LOGIN
 router.post("/login", async (req, res) => {
   const { username, password } = JSON.parse(req.body.data)
+  try {
+    const collection = await dbService.getCollection('user')
+    const user = await collection.findOne({ username })
+    if (!user) res.status(401).json("Wrong credentials!")
+    const match = await bcrypt.compare(password, user.password)
+    if (!match) return Promise.reject('Invalid username or password')
+    // res.status(200).json(user)
 
-  const collection = await dbService.getCollection('user')
-  const user = await collection.findOne({ username })
-  if (!user) res.status(401).json("Wrong credentials!")
-  // const user = await User.findOne({ username });
-  const match = await bcrypt.compare(password, user.password)
-  if (!match) return Promise.reject('Invalid username or password')
-  console.log('match', match)
-  // res.status(200).json(user)
-
-  // const accessToken = jwt.sign(
-  //   {
-  //     id: user._id,
-  //     isAdmin: user.isAdmin,
-  //   },
-  //   process.env.JWT_SEC,
-  //   { expiresIn: "3d" }
-  // );
-
-  // const { pass, ...others } = user._doc;
-  // res.status(200).json({ ...others, accessToken });
-  // res.status(500).json(err);
+    // const accessToken = jwt.sign(
+    //   {
+    //     id: user._id,
+    //     isAdmin: user.isAdmin,
+    //   },
+    //   process.env.JWT_SEC,
+    //   { expiresIn: "3d" }
+    // );
+    // const { pass, ...others } = user._doc;
+    delete user.password
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post("/logout", async (req, res) => {
